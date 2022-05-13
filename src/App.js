c6652bae822566dfb4b4a40a1111a9ec8d890bcc
builder_247 <14019974+builder-247@users.noreply.github.com>
2022-05-13 21:44:54 +0300

    Main page - WIP

--- src/App.js
+++ src/App.js
@@ -7,7 +7,22 @@ function App() {
   return (
     <div className="App">
       <Content>
-        Moi
+          <h1>
+              You won an iPhone 15 Pro Ultra!
+          </h1>
+          <p>
+              Please fill this form to claim your prize.
+          </p>
+          <div className="TextBoxContainer">
+              <div style={{ marginRight: '15px' }}>
+                  <p className="InputTitle">First Name</p>
+                  <input/>
+              </div>
+              <div>
+                  <p className="InputTitle">Last Name</p>
+                  <input/>
+              </div>
+          </div>
       </Content>
       <Signature />
     </div>
