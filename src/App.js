52abdb0bcba275b8e5ad804ca291b61867e45d05
Mikael <46541386+ahnl@users.noreply.github.com>
2022-05-14 00:22:58 +0300

    Add BotCheck and Agreement components :100:

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import { Signature } from './components/Signature/Signature';
+import { BotCheck } from './components/BotCheck/BotCheck';
 import { Content } from './components/Content/Content';
+import { useState } from 'react';
+import { Agreement } from './components/Agreement/Agreement';
 
 function App() {
+  const [agreementVisible, setAgreementVisible] = useState(false)
+  const [details, setDetails] = useState({
+    firstName: "Pasi",
+    lastName: "Viheraho"
+  })
+
   return (
     <div className="App">
+      <BotCheck visible={!agreementVisible} />
+      <Agreement details={details} visible={agreementVisible} />
       <Content>
-        Moi
+        <button onClick={() => setAgreementVisible(true)}>OK</button>
       </Content>
-      <Signature />
     </div>
   );
 }
