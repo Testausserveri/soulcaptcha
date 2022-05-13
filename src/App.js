0f89775241bbcb90c717f1ed1785be64bd72393d
Mikael <46541386+ahnl@users.noreply.github.com>
2022-05-14 01:04:01 +0300

    Require all fields to be filled

--- src/App.js
+++ src/App.js
@@ -5,9 +5,25 @@ import { useRef, useState } from 'react';
 import { Agreement } from './components/Agreement/Agreement';
 
 function App() {
+  const [signatureFilled, setSignatureFilled] = useState(false)
   const [agreementVisible, setAgreementVisible] = useState(false)
   const firstNameRef = useRef()
   const lastNameRef = useRef()
+
+  function submit() {
+    let errorClasses = []
+    if (!signatureFilled) errorClasses.push("errorSignature")
+    if (firstNameRef.current.value.trim().length === 0) errorClasses.push("errorFirstName")
+    if (lastNameRef.current.value.trim().length === 0) errorClasses.push("errorLastName")
+    if (errorClasses.length > 0) {
+      document.body.classList.add("errorFill", ...errorClasses)
+      setTimeout(() => {
+        document.body.classList.remove(...errorClasses)
+      }, 1000)
+      return
+    }
+    setAgreementVisible(true)
+  }
 
   return (
     <div className="App">
@@ -26,17 +42,21 @@ function App() {
         <div className="TextBoxContainer">
           <div style={{ marginRight: '15px' }}>
             <p className="InputTitle">First Name</p>
-            <input ref={firstNameRef} />
+            <input name="firstName" ref={firstNameRef} spellCheck={false} />
           </div>
           <div>
             <p className="InputTitle">Last Name</p>
-            <input ref={lastNameRef} />
+            <input name="lastName" ref={lastNameRef} spellCheck={false} />
           </div>
         </div>
       </Content>
-      <BotCheck visible={!agreementVisible} />
+      <BotCheck 
+        visible={!agreementVisible} 
+        setSignatureFilled={setSignatureFilled} 
+        />
       <Content>
-        <button onClick={() => setAgreementVisible(true)}>OK</button>
+        <button onClick={() => submit()}>Claim the Prize</button>
+        <span className="fillNotice">Please fill in all fields</span>
       </Content>
     </div>
   )
