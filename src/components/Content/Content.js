3e4f8ce02816ae58944ac2b12101df0fde7646bf
Mikael <46541386+ahnl@users.noreply.github.com>
2022-05-13 21:25:24 +0300

    Create Content & base for Signature -components

---
+++ src/components/Content/Content.js
@@ -0,0 +1,9 @@
+import "./Content.css"
+
+export function Content({children}) {
+    return (
+        <div className="content">
+            {children}
+        </div>
+    )
+}
---
+++ src/components/Signature/Signature.js
@@ -0,0 +1,12 @@
+import { Content } from "../Content/Content"
+import "./Signature.css"
+
+export function Signature() {
+    return (
+        <div className="signatureSection">
+            <Content>
+                Hello
+            </Content>
+        </div>
+    )
+}
